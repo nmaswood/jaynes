9eae7098f57314ae5d3055aba90434faaa9de8f6
Nasr Maswood <nasr@ironcladapp.com>
2019-03-04 11:04:07 -0800

    Init commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from typing import List
+
+from setuptools import find_packages, setup
+
+REQUIRED_PACKAGES: List[str] = []
+DEV_PACKAGES: List[str] = ['mypy', 'flake8', 'pytest']
+
+version = '0.0.1'
+
+setup(
+    name='jaynes',
+    version=version,
+    install_requires=REQUIRED_PACKAGES,
+    packages=find_packages(),
+    include_package_data=True,
+    description=('Probability utils'),
+    extras_require={'dev': DEV_PACKAGES},
+)
