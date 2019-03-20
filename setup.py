e450c41176e7fc626f04bbde53f85beb49f204cb
nmaswood <nmaswood@civisanalytics.com>
2019-03-19 21:57:46 -0700

    start work on parsing email data

--- setup.py
+++ setup.py
@@ -4,13 +4,18 @@ from setuptools import find_packages, setup
 
 REQUIRED_PACKAGES: List[str] = ['pymc3']
 DEV_PACKAGES: List[str] = ['mypy', 'flake8', 'pytest']
+GOOGLE_AUTH_PACKAGES: List[str] = [
+    'google-api-python-client',
+    'google-auth-httplib2',
+    'google-auth-oauthlib',
+]
 
 version = '0.0.1'
 
 setup(
     name='jaynes',
     version=version,
-    install_requires=REQUIRED_PACKAGES,
+    install_requires=REQUIRED_PACKAGES + GOOGLE_AUTH_PACKAGES,
     packages=find_packages(),
     include_package_data=True,
     description=('Probability utils'),
